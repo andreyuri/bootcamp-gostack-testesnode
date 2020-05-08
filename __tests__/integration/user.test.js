2d9d84f22613b870840bdbe22cef4f56342bccac
André Yuri <andre_yuri@live.com>
2020-05-08 18:21:40 -0300

    teste Criptografia de senha

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,11 +1,27 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
+
 import app from '../../src/app';
 
 import truncate from '../util/truncate';
 
+import User from '../../src/app/models/User';
+
 describe('User', () => {
   beforeEach(async () => {
     await truncate();
+  });
+
+  it('should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      name: 'Andre nascimento',
+      email: 'andre@nascimento.com.br',
+      password: '123456',
+    });
+
+    const compareHash = bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
   });
 
   it('should be able to register', async () => {
@@ -14,7 +30,7 @@ describe('User', () => {
       .send({
         name: 'Andre nascimento',
         email: 'andre@nascimento.com.br',
-        password_hash: '123456',
+        password: '123456',
       });
 
     expect(response.body).toHaveProperty('id');
@@ -26,7 +42,7 @@ describe('User', () => {
       .send({
         name: 'Andre nascimento',
         email: 'andre@nascimento.com.br',
-        password_hash: '123456',
+        password: '123456',
       });
 
     const response = await request(app)
@@ -34,7 +50,7 @@ describe('User', () => {
       .send({
         name: 'Andre nascimento',
         email: 'andre@nascimento.com.br',
-        password_hash: '123456',
+        password: '123456',
       });
 
     expect(response.status).toBe(400);
